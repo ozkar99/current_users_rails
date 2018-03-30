420f99a8eb0bd891c69b8657f1bc87d6bdf2fb0d
ozkar <ozkar@ozkar.org>
2018-03-29 22:30:46 -0600

    finishes PoC

--- app/assets/javascripts/channels/appereance.js
+++ app/assets/javascripts/channels/appereance.js
@@ -1,3 +1,1 @@
-console.log("Loading appereance.js")
-
 App.cable.subscriptions.create({ channel: "AppearanceChannel" });
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
 
 
   def self.currently_online
-    []
+    server = ActionCable.server
+    users = ActionCable.server.connections.map do |conn|
+      decoded = Base64.decode64 conn.connection_identifier
+      user_id = decoded.split('/').last
+      User.find(user_id)
+    end
+    users.uniq
   end
 end
